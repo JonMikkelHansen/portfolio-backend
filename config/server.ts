eace25cc5ef3f3289b2be71fa877b41bdf96f2b4
JonMikkelHansen <jon.mikkel.hansen@gmail.com>
2024-11-26 22:13:02 +0100

    another attempt

--- config/server.ts
+++ config/server.ts
@@ -1,12 +1,16 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 8080),
-  url: env('PUBLIC_URL', 'http://admin.jonmikkelhansen.dk'),
+  url: process.env.NODE_ENV === 'production'
+    ? env('PUBLIC_URL', 'http://admin.jonmikkelhansen.dk')
+    : 'http://localhost:8080',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
     },
-    path: '/admin',
+    url: process.env.NODE_ENV === 'production'
+      ? '/'
+      : '/admin',
   },
   app: {
     keys: env.array('APP_KEYS'),
